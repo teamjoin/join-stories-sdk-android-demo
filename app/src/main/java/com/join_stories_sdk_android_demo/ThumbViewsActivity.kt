1d0594038db841dc39a5d9c4c7fc168747c70b45
Max Lignereux <maxime.lignereux@teamjoin.fr>
2023-08-21 17:08:01 +0200

    update app demo with SDK 2.1.0

--- app/src/main/java/com/join_stories_sdk_android_demo/ThumbViewsActivity.kt
+++ app/src/main/java/com/join_stories_sdk_android_demo/ThumbViewsActivity.kt
@@ -2,17 +2,20 @@ package com.join_stories_sdk_android_demo
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.join_stories_sdk_android_demo.databinding.ActivityThumbBinding
+import com.joinstoriessdk.androidsdk.JoinStoriesAnalyticsCallback
+import com.joinstoriessdk.androidsdk.JoinStoriesEventType
 import com.joinstoriessdk.androidsdk.JoinStoriesListener
-import com.joinstoriessdk.androidsdk.R
 import com.joinstoriessdk.androidsdk.config.PlayerStandaloneAnimationOrigin
 import com.joinstoriessdk.androidsdk.config.PlayerVerticalAnchor
+import com.joinstoriessdk.androidsdk.data.model.AnalyticsWidgetValue
 import com.joinstoriessdk.androidsdk.ui.player.OnDismissState
 
-class ThumbViewsActivity : AppCompatActivity(), JoinStoriesListener {
+class ThumbViewsActivity : AppCompatActivity(), JoinStoriesListener, JoinStoriesAnalyticsCallback {
 
     private lateinit var binding: ActivityThumbBinding
 
@@ -25,7 +28,7 @@ class ThumbViewsActivity : AppCompatActivity(), JoinStoriesListener {
         binding.storyPlayer.apply {
             playerVerticalAnchor = PlayerVerticalAnchor.CENTER
             animationOrigin = PlayerStandaloneAnimationOrigin.BOTTOM
-            playerBackgroundColor = Color.TRANSPARENT
+            playerBackgroundColor = Color.parseColor("#33AAFFBB")
             playerShowShareButton = true
             playerClosingButton = true
             playerCornerRadius = 30f
@@ -39,10 +42,10 @@ class ThumbViewsActivity : AppCompatActivity(), JoinStoriesListener {
             loaderInnerViewColor = intArrayOf(Color.RED, Color.CYAN, Color.YELLOW)
             loaderColors = intArrayOf(Color.GREEN)
             joinStoriesListener = this@ThumbViewsActivity
+            analyticsCallback = this@ThumbViewsActivity
         }
         binding.storyScaffold.setStoryPlayer(binding.storyPlayer)
         binding.storyScaffold.startThumbView("widget-sdk-test-thumb")
-
 
         binding.secondThumbView.apply {
             withLabel = true
@@ -51,6 +54,7 @@ class ThumbViewsActivity : AppCompatActivity(), JoinStoriesListener {
             thumbViewOverlayColor = Color.parseColor("#BB4F4C")
             loaderInnerViewColor = intArrayOf(Color.LTGRAY, Color.GREEN, Color.YELLOW)
             loaderInnerViewColor = intArrayOf(Color.RED, Color.BLUE)
+            analyticsCallback = this@ThumbViewsActivity
         }
         binding.secondThumbView.setStoryPlayer(binding.storyPlayer)
         binding.secondThumbView.startThumbView("widget-sdk-test-standalone")
@@ -93,6 +97,17 @@ class ThumbViewsActivity : AppCompatActivity(), JoinStoriesListener {
                 Toast.makeText(this@ThumbViewsActivity, "Dismiss manual", Toast.LENGTH_SHORT)
                     .show()
             }
+        }
+    }
+
+    override fun onAnalyticsCallback(event: JoinStoriesEventType, data: AnalyticsWidgetValue) {
+        when(event) {
+            JoinStoriesEventType.AdditionalClickOnWidget -> Log.d("ThumbViewsActivity", "AdditionalClickOnWidget")
+            JoinStoriesEventType.ComponentVisible50 -> Log.d("ThumbViewsActivity", "ComponentVisible50")
+            JoinStoriesEventType.ComponentVisible75 -> Log.d("ThumbViewsActivity", "ComponentVisible75")
+            JoinStoriesEventType.FirstClickOnWidget -> Log.d("ThumbViewsActivity", "FirstClickOnWidget")
+            JoinStoriesEventType.StoriesFetched -> Log.d("ThumbViewsActivity", "StoriesFetched")
+            JoinStoriesEventType.WidgetMounted -> Log.d("ThumbViewsActivity", "WidgetMounted")
         }
     }
 }
